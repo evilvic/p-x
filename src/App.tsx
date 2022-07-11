7d8a36f31d757593d0717a813ad6a675294a2384
Víctor Peña Romero <vic.pero@icloud.com>
2022-07-11 07:05:33 -0500

    test blockly

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,54 @@
+import { useEffect, useRef } from 'react'
 
-function App() {
+import Blockly, { Toolbox } from 'blockly'
+
+const App = () => {
+
+  const toolbox = {
+    "kind": "flyoutToolbox",
+    "contents": [
+      {
+        "kind": "block",
+        "type": "controls_if"
+      },
+      {
+        "kind": "block",
+        "type": "controls_repeat_ext"
+      },
+      {
+        "kind": "block",
+        "type": "logic_compare"
+      },
+      {
+        "kind": "block",
+        "type": "math_number"
+      },
+      {
+        "kind": "block",
+        "type": "math_arithmetic"
+      },
+      {
+        "kind": "block",
+        "type": "text"
+      },
+      {
+        "kind": "block",
+        "type": "text_print"
+      },
+    ]
+  }
+
+  useEffect(() => {
+    const workspace  = Blockly.inject('blocklyDiv', { toolbox });
+  }, [])
 
   return (
     <>
-      <canvas className="rounded-md bg-slate-50 w-[500px] h-[500px]"/>
+    <div 
+      id='blocklyDiv'
+      className='w-[480px] h-[600px]'
+    />
+      <canvas className='rounded-md bg-slate-50 w-[500px] h-[500px]'/>
     </>
   )
 
